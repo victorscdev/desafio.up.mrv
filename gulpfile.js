51000c14f7ffc4108c25347a2e16c3a7889d6a79
Victor Costa <victor@fasters.com.br>
2022-01-22 21:03:14 -0300

    feat: Create automator gulp for js

--- gulpfile.js
+++ gulpfile.js
@@ -6,10 +6,16 @@ const autoprefixer = require('gulp-autoprefixer');
 const minifycss = require('gulp-minify-css');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
+const concat = require('gulp-concat');
+const terser = require('gulp-terser');
 const log = util.log;
-const watcher = watch([`./public/assets/scss/**/*.scss`]);
+const watcherSCSS = watch([
+	`./public/assets/scss/**/*.scss`,
+	`./src/**/*.scss`,
+]);
+const watcherJS = watch([`./src/**/*.js`]);
 
-function sass_to_css() {
+function scss_task() {
 	log(`Generate CSS files ${new Date().toString()}`);
 	src(`./public/assets/scss/import.scss`)
 		.pipe(sourcemaps.init())
@@ -17,13 +23,26 @@ function sass_to_css() {
 		.pipe(autoprefixer('last 3 version', 'safari 5', 'ie 8', 'ie 9'))
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(minifycss())
-		.pipe(sourcemaps.write())
+		.pipe(sourcemaps.write('.'))
 		.pipe(dest(`./public/assets/css`));
 }
 
+function javascrip_task() {
+	log(`Generate JS files ${new Date().toString()}`);
+	src([`./public/assets/js/html-include.js`, `./src/**/*.js`])
+		.pipe(sourcemaps.init())
+		.pipe(concat(`main.bundle.min.js`))
+		.pipe(terser())
+		.pipe(sourcemaps.write('.'))
+		.pipe(dest(`./public/assets/js`));
+}
+
 function watch_gulp() {
-	watcher.on('change', function () {
-		sass_to_css();
+	watcherSCSS.on('change', function () {
+		scss_task();
+	});
+	watcherJS.on('change', function () {
+		javascrip_task();
 	});
 }
 
---
+++ src/components/card/_card.js
@@ -0,0 +1,4 @@
+function getAlert() {
+	alert('Teste');
+	console.log('teste');
+}
